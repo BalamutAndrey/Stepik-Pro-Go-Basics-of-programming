70d44e4791a93ea17d3aa0990eec544661f399c7
Andrey Balamut <balamut.andrey@gmail.com>
2024-02-10 18:59:59 +0300

    Add 2.4.6 subject and final code

---
+++ 2.4.6. Dividing the apples - 2/src/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scaner := bufio.NewScanner(os.Stdin)
+	_ = scaner.Scan()
+	n, _ := strconv.Atoi(scaner.Text())
+	_ = scaner.Scan()
+	k, _ := strconv.Atoi(scaner.Text())
+	result := k % n
+	fmt.Println(result)
+}
