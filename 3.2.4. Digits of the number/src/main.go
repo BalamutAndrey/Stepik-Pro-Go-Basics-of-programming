bc0780c040f95d8d749a8af083a757446d44c080
Andrey Balamut <balamut.andrey@gmail.com>
2024-07-14 08:28:59 +0300

    Add 3.2.4 final code

--- 3.2.4. Digits of the number/src/main.go
+++ 3.2.4. Digits of the number/src/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var num, a, b, c int
+	fmt.Scan(&num)
+	c = num % 10
+	num /= 10
+	b = num % 10
+	a = num / 10
+	if a != b && a != c && b != c {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
 }
